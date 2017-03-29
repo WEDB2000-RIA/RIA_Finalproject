f2b456d6a727ab856ff086e27b5a9bdb60f2f7cc
Minsu Lee <w0293156@nscc.ca>
2017-03-29 17:56:16 -0300

    Update template based on naming discussion

---
+++ Project/js/backscripts.js
@@ -0,0 +1,38 @@
+var sortBy;
+var orderBy;
+var isEdit = false;
+var items;	  // For whole data
+var item;     // For selected item
+
+//Sorting and Visual Effects Assigned to Minsu
+$(function () {
+	readProducts(sortBy, orderBy);
+}
+
+//Assigned to Yan
+//readProducts Function will load data from server and make table
+function readProducts(sortBy, orderBy){
+
+}
+
+//Assigned to Yan
+//createUpdateItem Function will create and update data and store to server
+
+//Validation Assigned to Minsu
+function createUpdateItem(){
+
+}
+
+//Assigned to Yan
+//loadItem Function will load data to Form for update when edit button clicked 
+function loadItem(i){
+
+}
+
+//Assigned to Yan
+//deleteItem Function will delete data 
+function deleteItem(){ // i is for get index
+
+}
+
+//Dialog Assigned to Minsu
---
+++ Project/js/frontscripts.js
@@ -0,0 +1,19 @@
+var items;	  // For whole data
+var item;     // For selected item
+var category; // For sorting
+
+//Assigned to Paddy
+$(function () {
+    loadProducts(category);
+}
+
+//Assigned to Paddy
+//loadProducts Function will load image and name of all products from Server 
+function loadProducts(sortBy){
+	}
+
+//Assigned to Paddy
+//loadItem Function will load data when user pick the image on product
+function loadItem(i){
+
+}
