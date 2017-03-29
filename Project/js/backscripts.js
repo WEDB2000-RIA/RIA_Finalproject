e4c02f50bf394c66bda8a19f85b5760d450732c3
Minsu Lee <w0293156@nscc.ca>
2017-03-29 10:16:43 -0300

    Commit after scrum minutes 3

---
+++ Project/js/backscripts.js
@@ -0,0 +1,37 @@
+var sortBy;
+var orderBy;
+
+$(function () {
+	readProducts(sortBy, orderBy);
+
+	//Assigend to Minsu and Paddy
+    //Sortig functions goes here
+
+    //End of Sortig functions
+
+    //Assigned to Minsu and Paddy
+    //Validations goes here
+
+    //End of validations
+}
+
+//Assigned to Yan
+//Read operation
+function readProducts(sortBy, orderBy){
+
+}
+
+//Assigned to Yan validation would be added by Minsu
+//Create Update operations
+function createUpdateItem(){
+
+}
+
+//Assigned to Yan
+//Delete operation
+function deleteItem(){ 
+
+}
+
+//Assigned to Minsu
+// Dialog function goes here
---
+++ Project/js/frontscripts.js
@@ -0,0 +1,20 @@
+$(function () {
+    loadProducts(sortBy);
+
+    //Assigend Minsu and Paddy
+    //Sortig functions goes here
+
+    //End of Sortig functions
+}
+
+//Assigned to Paddy
+//Load all products from server
+function loadProducts(sortBy){
+
+}
+
+//Assigned to Paddy
+//Load a product from server when user click the image from product
+function loadItem(i){
+
+}
