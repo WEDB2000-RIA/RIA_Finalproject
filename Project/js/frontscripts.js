b4f0ff69761f5999e3ef67a56f030b7577650dee
Paddy Vaughan <w0277831@nscc.ca>
2017-03-31 11:06:44 -0300

    load products

--- Project/js/frontscripts.js
+++ Project/js/frontscripts.js
@@ -5,6 +5,35 @@ var category; // For sorting
 //Assigned to Paddy
 $(function () {
     loadProducts(category);
+}function loadProducts(sortBy){
+    $("#spinner").removeClass("hidden");
+    $.ajax({
+        type:"GET",
+        url: "http://localhost:3000/products?"+category,
+        dataType:"JSON",
+        //cache : false, 
+        success: function(data) {
+            items = data;
+            $("#spinner").addClass("hidden");
+            $("#products").html(""); //initialize
+            for (var i = 0; i < data.length; i++) {
+            //alert(data[i].image)
+            var product =	`<div class="col-sm-3 col-xs-6">` +
+                              	`<a  href='#' onclick="loadItem(`+i+`)">` +
+                                	`<img class="img-responsive portfolio-item" src="./img/${data[i].image}">` +
+                                `</a>`	+
+                                `<h4>${data[i].name}</h4>` +
+                                //`<p>${data[i].price}</p>`	+
+                        	`<hr><hr></div>`;
+            $("#products").append(product);
+            }   
+
+        },
+        error: function(error) {
+            $("$spinner").addClass("hidden");
+            alert("There was an error retrieving the data.");
+        }
+    }); // ajax
 }
 
 //Assigned to Paddy
